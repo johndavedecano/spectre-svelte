b8181a8878ae293e56c1a2d595d44537f4c885bd
John Dave Decano <johndavedecano@gmail.com>
2021-12-10 09:48:04 +0800

    initial commit

---
+++ src/elements/Button.stories.svelte
@@ -0,0 +1,90 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Button from "./Button.svelte";
+</script>
+
+<Meta title="Elements/Button" component={Button} />
+
+<Template let:args>
+  <Button {...args} on:click={args.onClick} />
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    label: "Default",
+  }}
+/>
+
+<Story
+  name="Primary"
+  args={{
+    label: "Primary",
+    variant: "primary",
+  }}
+/>
+
+<Story
+  name="Success"
+  args={{
+    label: "Success",
+    variant: "success",
+  }}
+/>
+
+<Story
+  name="Error"
+  args={{
+    label: "Error",
+    variant: "error",
+  }}
+/>
+
+<Story
+  name="Link"
+  args={{
+    label: "Link",
+    variant: "link",
+  }}
+/>
+
+<Story
+  name="Small"
+  args={{
+    label: "Small",
+    size: "sm",
+  }}
+/>
+
+<Story
+  name="Large"
+  args={{
+    label: "Large",
+    size: "lg",
+  }}
+/>
+
+<Story
+  name="Loading"
+  args={{
+    label: "Loading",
+    loading: true,
+  }}
+/>
+
+<Story
+  name="Disabled"
+  args={{
+    label: "Disabled",
+    disabled: true,
+    variant: "primary",
+  }}
+/>
+
+<Story
+  name="Circle Icon"
+>
+  <Button variant="primary" action circle>
+    <i class="icon icon-arrow-left" />
+  </Button>
+</Story>
---
+++ src/elements/Button.svelte
@@ -0,0 +1,81 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let label = "";
+
+  export let variant = "default";
+
+  export let size = "md";
+
+  export let type = "button";
+
+  export let loading = false;
+
+  export let disabled = false;
+
+  export let circle = false;
+
+  export let action = false;
+
+  const variants = {
+    primary: "btn-primary",
+    link: "btn-link",
+    success: "btn-success",
+    error: "btn-error",
+  };
+
+  const sizes = {
+    sm: "btn-sm",
+    md: undefined,
+    lg: "btn-lg",
+  };
+
+  const dispatch = createEventDispatcher();
+
+  /**
+   * Optional click handler
+   */
+  function onClick(event) {
+    dispatch("click", event);
+  }
+
+  $: getComputedClasses = () => {
+    const classes = ["btn"];
+
+    if (variants[variant]) {
+      classes.push(variants[variant]);
+    }
+
+    if (sizes[size]) {
+      classes.push(sizes[size]);
+    }
+
+    if (loading) {
+      classes.push("loading");
+    }
+
+    if (disabled) {
+      classes.push("disabled");
+    }
+
+    if (action) {
+      classes.push("btn-action");
+    }
+
+    if (circle) {
+      classes.push("s-circle");
+    }
+
+    return classes.join(" ");
+  };
+</script>
+
+<button
+  {...$$props}
+  {type}
+  {disabled}
+  class={getComputedClasses()}
+  on:click={onClick}
+>
+  <slot>{label}</slot>
+</button>
