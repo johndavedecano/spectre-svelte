ecef7ba8a7de15ca140b3b0c391aef06d15c1b6d
John Dave Decano <johndavedecano@gmail.com>
2021-12-11 17:51:40 +0800

    added hero

---
+++ src/layout/Hero.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let wrapperClasses = "";
+
+  export let bodyClasses = "";
+
+  export let size = "";
+
+  $: getWrapperClasses = () => {
+    const classes = [];
+
+    if (size) {
+      classes.push(`hero-${size}`);
+    }
+
+    if (wrapperClasses) {
+      classes.push(wrapperClasses);
+    }
+
+    return classes.join(" ");
+  };
+
+  $: getBodyClasses = () => {
+    const classes = ["hero-body"];
+
+    if (bodyClasses) {
+      classes.push(bodyClasses);
+    }
+
+    return classes.join(" ");
+  };
+</script>
+
+<div {...$$restProps} class={getWrapperClasses()}>
+  <div class={getBodyClasses()}>
+    <slot />
+  </div>
+</div>
